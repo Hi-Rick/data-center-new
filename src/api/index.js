61427f4b036bea8738faec4e2232470360b8d816
ymq <king3410@qq.com>
2020-12-10 22:18:28 +0800

    链接变动

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import {get,post} from  '@/utils/request'
 
-export let getAllAsset = (query)=> get('http://47.104.100.75/smartwater/data/data_asset/getAllAssetList',query)
+export let getAllAsset = (query)=> get('http://222.173.76.3:65001/smartwater/data/data_asset/getAllAssetList',query)
 
-export let getTreeList = (query)=> get('http://47.104.100.75/smartwater/data/data_asset/getAssetTreeList',query)
+export let getTreeList = (query)=> get('http://222.173.76.3:65001/smartwater/data/data_asset/getAssetTreeList',query)
 
